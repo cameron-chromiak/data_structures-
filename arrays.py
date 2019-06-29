0d24d18e5f3c9a7e2a3c895cb9a789996c1116c6
cameron-chromiak <cam.chromiak@hotmail.com>
2019-06-29 15:11:43 -0400

    refactor

--- arrays.py
+++ arrays.py
@@ -1,29 +1,24 @@
 import datetime
 import random
 
+bubble_stats =[]
 stats = {}
-def calc_run_time_stats(start, end, length, algorithm):
-    #do stuff with algorithm name
-    run_time = end - start
-    data_pair = {length: run_time}
-    if (algorithm == 'bubble_sort'):
-        if ('bubble_sort' not in stats):
-            stats['bubble_sort']['runtime'] = run_time
-    print(stats)
+def calc_run_time_stats(start, end):
     return end - start
+          
 
 def merge_sort_array(arr):
     if len(arr) >1: 
-        mid = len(arr)//2 #Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
+        mid = len(arr)//2
+        L = arr[:mid] 
+        R = arr[mid:]
 
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
+        mergeSort(L)
+        mergeSort(R)
 
         i = j = k = 0
           
-        # Copy data to temp arrays L[] and R[] 
+
         while i < len(L) and j < len(R): 
             if L[i] < R[j]: 
                 arr[k] = L[i] 
@@ -53,16 +48,16 @@ def bubble_sort_array(arr):
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     end_time = timer()
-    run_time = calc_run_time_stats(start_time, end_time, arr_len, 'bubble_sort')
-    print('')
-    print('Bubble Sort:',run_time)
-
+    run_time = calc_run_time_stats(start_time, end_time)
+    bubble_stats.append(run_time)
 
 def timer():
-    current_time = datetime.datetime.now()
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
     return current_time
 
 
+#generate random array max_runs times with 500-max_right_val size
 def fill_array(max_runs, max_right_val):
                               
     for i in range(max_runs):
@@ -74,7 +69,7 @@ def fill_array(max_runs, max_right_val):
 
 
 def main():
-    fill_array(30, 1500)
-    
+    fill_array(3, 1500)
+    print(bubble_stats)
     
 main()
